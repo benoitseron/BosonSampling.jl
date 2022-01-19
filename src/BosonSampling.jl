23434c5468465dea44cd1004a62583ffb364de33
benoitseron <benoitseron@gmail.com>
2022-01-19 21:02:26 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.23

---
+++ src/BosonSampling.jl
@@ -0,0 +1,5 @@
+module BosonSampling
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BosonSampling
+using Test
+
+@testset "BosonSampling.jl" begin
+    # Write your tests here.
+end
