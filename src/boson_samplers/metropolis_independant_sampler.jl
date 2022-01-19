3a05a2436f43383404730e1dea96dca564928af9
benoitseron <benoitseron@gmail.com>
2022-01-20 00:10:03 +0100

    MIS ex usage

--- src/boson_samplers/metropolis_independant_sampler.jl
+++ src/boson_samplers/metropolis_independant_sampler.jl
@@ -59,22 +59,3 @@ function metropolis_sampler(;target_pdf, known_pdf, known_sampler, starting_stat
     samples
 
 end
-
-###### sampling tests ######
-
-n = 8
-m = n^2
-starting_state = zeros(Int, m)
-input_state = first_modes_array(n,m)
-
-U = copy(rand_haar(m))
-
-# generate a collisionless state as a starting point
-starting_state = iterate_until_collisionless(() -> random_occupancy(n,m))
-
-known_pdf(state) = process_probability_distinguishable(U, input_state, state)
-target_pdf(state) = process_probability(U, input_state, state)
-known_sampler = () -> iterate_until_collisionless(() -> classical_sampler(U = U, m = m, n = n)) # gives a classical sampler
-
-
-samples = metropolis_sampler(;target_pdf = target_pdf, known_pdf = known_pdf , known_sampler = known_sampler , starting_state = starting_state, n_iter = 100)
--- src/example_usage.jl
+++ src/example_usage.jl
@@ -26,6 +26,25 @@ ppd = full_bunching_probability(interf, ipd, subset_modes)
 @test pb/ppd > 1. # this doesn't HAVE TO pass but will pass in nearly all
 # cases
 
-### sampling ###
+### classical sampling ###
 
 classical_sampler(U = rand_haar(16), m = 16, n = 3)
+
+### MIS sampling ###
+
+n = 8
+m = n^2
+starting_state = zeros(Int, m)
+input_state = first_modes_array(n,m)
+
+U = copy(rand_haar(m))
+
+# generate a collisionless state as a starting point
+starting_state = iterate_until_collisionless(() -> random_occupancy(n,m))
+
+known_pdf(state) = process_probability_distinguishable(U, input_state, state)
+target_pdf(state) = process_probability(U, input_state, state)
+known_sampler = () -> iterate_until_collisionless(() -> classical_sampler(U = U, m = m, n = n)) # gives a classical sampler
+
+
+samples = metropolis_sampler(;target_pdf = target_pdf, known_pdf = known_pdf , known_sampler = known_sampler , starting_state = starting_state, n_iter = 100)
