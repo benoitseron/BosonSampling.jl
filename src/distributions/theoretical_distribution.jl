e6830887ef6023c5ce58e70a9950d09e86d63c95
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2022-02-03 12:03:47 +0100

    Add files via upload

---
+++ src/distributions/theoretical_distribution.jl
@@ -0,0 +1,34 @@
+function theoretical_distribution(;input::Input, distinguishability::Real, interf::Interferometer, gram_matrix::GramMatrix, i=nothing)
+
+    input_modes = input.r.state
+    number_photons = input.r.n
+    number_modes = input.r.m
+    U = interf.U
+    S = gram_matrix.S
+
+    input_event = fill_arrangement(input_modes)
+    output_events = generate_events(number_modes, number_photons)
+
+    function compute_pi(event)
+
+        M = U[input_event, event]
+        output_modes = mode_occupacy_to_occupancy_vector(event, number_modes)
+
+        if distinguishability == 1 ||distinguishability == 0
+            return abs(ryser_fast(M)).^2
+        else
+            return multi_dim_ryser(M, S)
+        end
+
+    end
+
+    complete_distribution() = [compute_pi(e)/factorial(number_photons) for e in output_events]
+
+    if i == nothing
+        return complete_distribution()
+    else
+        return compute_pi(output_events[i])
+
+    end
+
+end
