392c2b381322851719c2c3f7a846d171112604d0
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2022-02-03 12:07:28 +0100

    Create hpsd.jl

---
+++ src/hpsd.jl
@@ -0,0 +1,21 @@
+# hermitian positive semi definite matrices
+# follows https://arxiv.org/abs/1609.02416
+
+function sample_H_matrix(n::Int, m::Int)
+
+    """samples a unitary of size m according to the Haar measure and returns the
+    H matrix corresponding to n photons sent going from the first n top input
+    modes to the top n output modes"""
+
+    U = rand_haar(m)
+
+    input_state = [i <= n ? 1 : 0 for i in 1:m]
+    output_state = input_state # variations of this are implicit in the Haar random sampling
+
+    H_matrix(U, input_state, output_state)
+
+end
+
+### example ###
+
+# @test is_positive_semidefinite(sample_H_matrix(5,4))
