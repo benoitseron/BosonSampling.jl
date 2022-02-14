f20ed62864e77de5e7d386b7e004dee8a7f7fb41
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2022-02-14 21:44:12 +0100

    fixed LaplaceExpansion

--- src/tools.jl
+++ src/tools.jl
@@ -1,18 +1,31 @@
-function collect_sub_mat_perm(U)
+function LaplaceExpansion(perm_mat, full_mat)
 
-    if size(U)[1] == 1 || size(U)[2] == 1
-        return U
-    else
-        return [fast_glynn_perm(remove_row_col(U, [], [i])) for i in 1:size(U)[2]]
+    function collect_sub_mat_perm(A)
+
+        if length(size(A)) == 1
+            v = [(A...)...]
+            return [v[setdiff(1:end)] for i in 1:length(v)]
+        else
+            sub_mat = [remove_row_col(A, [], [i]) for i in 1:size(A)[2]]
+            return [fast_glynn_perm(a) for a in sub_mat]
+        end
+
     end
 
-end
+    res = []
+    v_perms = collect_sub_mat_perm(perm_mat)
 
-function LaplaceExpansion(perm_mat, full_mat)
-    permanents = collect_sub_mat_perm(perm_mat)
-    row_ = [full_mat[i,:] for i in 1:size(full_mat)[1]]
-    res = [dot(row, permanents) for row in row_]
+    for i in 1:size(full_mat)[1]
+        rowA = full_mat[i,:]
+        ans = 0
+        for j in 1:length(v_perms)
+            ans += rowA[j] * v_perms[j]
+        end
+        push!(res, ans)
+    end
+
     return [abs(r)^2 for r in res]
+
 end
 
 function total_variation_distance(p, q)
@@ -25,4 +38,9 @@ function total_variation_distance(p, q)
 
     return sum(abs(p[i]-q[i]) for i = 1:length(p))
 
+end
+
+function collect_sub_mat_perm(U)
+    sub_mat = [remove_row_col(U, [], [i]) for i in 1:size(U)[2]]
+    return [fast_glynn_perm(m) for m in sub_mat]
 end
