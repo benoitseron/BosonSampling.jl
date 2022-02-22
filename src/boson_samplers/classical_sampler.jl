fb425f210708f0a5e443218a7dec73dff5d3e4d3
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2022-02-22 21:29:09 +0100

    Update classical_sampler.jl

--- src/boson_samplers/classical_sampler.jl
+++ src/boson_samplers/classical_sampler.jl
@@ -1,27 +1,3 @@
-# function classical_sampler(;input::Input, interf::Interferometer)
-#
-#     """outputs a sample of output mode of the classical distribution
-#     (distinguishable particles) form the input of [1^n O^(m-n)]
-#     according to section 8.1 of https://arxiv.org/abs/1309.7460"""
-#
-#     input_modes = input.r.state
-#     n = input.r.n
-#     m = input.r.m
-#
-#     output_state = zeros(Int, m)
-#     output_modes = collect(1:m)
-#
-#     U = interf.U
-#
-#     for j in 1:n
-#         this_output_mode = wsample(output_modes, abs.(U[j,:]).^2)
-#         output_state[this_output_mode] += 1
-#     end
-#
-#     output_state
-#
-# end
-
 function classical_sampler(U::Matrix, n::Int, m::Int)
 
     output_state = zeros(Int,m)
@@ -38,8 +14,8 @@ function classical_sampler(U::Matrix, n::Int, m::Int)
     output_state
 end
 
-function classical_sampler(input::Input, interf::Interferometer)
+function classical_sampler(;input::Input, interf::Interferometer)
 
-    return classical_sampler(U=interf.U, n=input.r.n, m=input.r.m)
+    return classical_sampler(interf.U, input.r.n, input.r.m)
 
 end
