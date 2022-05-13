1f4d64d9343e577ec513d05130baaed0684769b9
benoitseron <benoitseron@gmail.com>
2022-05-13 18:58:47 +0200

    make docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,52 @@
 using Documenter
 using BosonSampling
 
+About = "Introduction" => "intro.md"
+
+# GettingStarted = "gettingstarted.md"
+
+Functions = "Functions" => "index.md"
+
+#
+
+# Examples = "Examples" => [
+
+#         "examples/flux.md"
+
+#     ]
+
+#
+
+# License = "License" => "license.md"
+
+PAGES = [About,  Functions]
+
+
+
 makedocs(
-    sitename = "BosonSampling",
+
+    sitename = "BosonSampling.jl",
+
+    modules = [BosonSampling],
+
+    authors = "Benoit Seron, Antoine Restivo",
+
     format = Documenter.HTML(),
-    modules = [BosonSampling]
+
+    pages = PAGES
+
 )
 
+# deploydocs(repo = "github.com/Evizero/Augmentor.jl.git")
+
 # Documenter can also automatically deploy documentation to gh-pages.
+
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
+
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+
+deploydocs(
+
+    repo = "github.com/benoitseron/BosonSampling.jl.git"
+
+)
