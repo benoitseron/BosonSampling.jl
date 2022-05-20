28fbd5d812757bf3d1df03da3397930c3aa0484d
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2022-05-20 13:13:39 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,12 +10,24 @@ makedocs(
     format = Documenter.HTML(prettyurls = false),
     pages = [
         "About" => "about.md",
-        "Types" => Any[
-            "inputs" => "types/input.md",
-            "events" => "types/events.md"],
-        "Functions" => Any[
-            "bunching" => "functions/bunching.md"
-        ]
+        "Library" => Any[
+            "Types" => Any[
+                "inputs" => "types/input.md",
+                "events" => "types/events.md",
+                "interferomters" => "types/interferometers.md",
+                "measurements" => "types/measurements.md",
+                "partitions" => "types/partitions.md",
+                "type utilities" => "types/type_functions.md"],
+            "Functions" => Any[
+                "certification" => "functions/bayesian.md",
+                "bunching" => "functions/bunching.md",
+                "distributions" => "functions/distributions.md",
+                "partitions" => "functions/partitions.md",
+                "tools" => "functions/proba_tools.md",
+                "samplers" => "functions/samplers.md",
+                "scattering" => "functions/scattering.md",
+                "special_matrices" => "functions/special_matrices.md"]
+                ]
     ]
 )
 
