ed431ef0e7e8784971f3e4da557c48a900ee5f68
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2022-05-20 12:47:13 +0200

    Update interferometers.jl

--- src/types/interferometers.jl
+++ src/types/interferometers.jl
@@ -2,6 +2,11 @@
 
 abstract type Interferometer end
 
+"""
+    UserDefinedInterferometer(U::Matrix)
+
+Creates an instance of `Interferometer` from a provided unitary matrix ``U``.
+"""
 struct UserDefinedInterferometer <: Interferometer
 
     #actively checks unitarity, inefficient if outputing many matrices that are known to be unitary
@@ -10,24 +15,44 @@ struct UserDefinedInterferometer <: Interferometer
     UserDefinedInterferometer(U) = is_unitary(U) ? new(size(U,1), U) : error("input matrix is non unitary")
 end
 
+"""
+    RandHaar(m::Int)
+
+Creates an instance of `Interferometer` from a Haar distributed unitary matrix of dimension ``m``.
+"""
 struct RandHaar <: Interferometer
     m::Int
     U::Matrix{ComplexF64}
     RandHaar(m) = new(m,rand_haar(m))
 end
 
+"""
+    Fourier(m::Int)
+
+Creates a Fourier `Interferometer` of dimension ``m``.
+"""
 struct Fourier <: Interferometer
     m::Int
     U::Matrix{ComplexF64}
     Fourier(m::Int) = new(m,fourier_matrix(m))
 end
 
+"""
+    Hadamard(m::Int)
+
+Creates a Hadamard `Interferometer` of dimension ``m``.
+"""
 struct Hadamard <: Interferometer
     m::Int
     U::Matrix
     Hadamard(m::Int) = new(m,hadamard_matrix(m))
 end
 
+"""
+    BeamSplitter(transmission_amplitude::Float64)
+
+Creates a beam-splitter with tunable transmissivity.
+"""
 struct BeamSplitter <: Interferometer
     transmission_amplitude::Float64
     U::Matrix
@@ -35,6 +60,11 @@ struct BeamSplitter <: Interferometer
     BeamSplitter(transmission_amplitude) = new(transmission_amplitude, beam_splitter(transmission_amplitude),2)
 end
 
+"""
+    Rotation(angle::Float64)
+
+Creates a Rotation matrix with tunable angle.
+"""
 struct Rotation <: Interferometer
     angle::Float64
     U::Matrix
@@ -42,6 +72,11 @@ struct Rotation <: Interferometer
     Rotation(angle) = new(angle, rotation_matrix(angle),2)
 end
 
+"""
+    PhaseShift(shifted_modes::Array, param_::Array)
+
+Creates a phase-shifter that is applied on the modes precised by shifted_modes with phase shifts given in param_.    
+"""
 struct PhaseShift <: Interferometer
     shifted_modes::Array
     param_::Array
