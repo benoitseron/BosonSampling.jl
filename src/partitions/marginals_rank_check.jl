0dc33ad7da40e7239c4fc576858f06a4b8805524
benoitseron <benoitseron@gmail.com>
2022-07-26 18:03:45 +0200

    mistake in marginals check

--- src/partitions/marginals_rank_check.jl
+++ src/partitions/marginals_rank_check.jl
@@ -21,10 +21,6 @@ using LinearRegression
 
 function foo(n, r_s, r_z)
 
-    n = 5
-    r_s = 4
-    r_z = 3
-
     U = copy(rand_haar(n))
 
     S = rand_gram_matrix_rank(n,r_s)
@@ -41,11 +37,11 @@ function foo(n, r_s, r_z)
 
     pr = convert(Matrix{ComplexF64}, pr)
 
-    @argcheck rank(pr) == r_z "doesn't hold"
+    @show rank(pr .* S)
 
 end
 
 
-for i in 1:10000
-    foo(10,4,2)
+for i in 1:1
+    foo(10,2,2)
 end
