c80c0d9386a1100f6a36521c9fda9bcd414285e9
benoitseron <benoitseron@gmail.com>
2022-08-24 17:53:29 +0200

    fixed dark counts code sample

--- docs/publication/package/dark_counts.jl
+++ docs/publication/package/dark_counts.jl
@@ -12,9 +12,13 @@ end
 # works DarkCountFockSample(0.01)
 # fails DarkCountFockSample(-1)
 
-# define the sampling algorithm
-function sample!(ev::Event{TIn, TOut}) where {TIn<:InputType, TOut <: DarkCountFockSample}
-
+# define the sampling algorithm:
+# the function sample! is modified to take into account
+# the new measurement type
+# this allows to keep the same syntax and in fact reuse
+# any function that would have previously used sample!
+# at no cost
+function BosonSampling.sample!(ev::Event{TIn, TOut}) where {TIn<:InputType, TOut <: DarkCountFockSample}
 
     # sample without dark counts
     ev_no_dark = Event(ev.input_state, FockSample(), ev.interferometer)
@@ -22,17 +26,18 @@ function sample!(ev::Event{TIn, TOut}) where {TIn<:InputType, TOut <: DarkCountF
     sample_no_dark = ev_no_dark.output_measurement.s
 
     # now, apply the dark counts to "perfect" samples
-
     observe_dark_count(p) = Int(do_with_probability(p)) # 1 with probability p, 0 with probability 1-p
     dark_counts = [observe_dark_count(ev.output_measurement.p) for i in 1: ev.input_state.m]
 
     ev.output_measurement.s = sample_no_dark + dark_counts
 end
 
+### example ###
+
+# experiment parameters
 n = 10
 m = 10
 p_dark = 0.1
-# experiment parameters
 input_state = first_modes(n,m)
 interf = RandHaar(m)
 i = Input{Bosonic}(input_state)
@@ -40,38 +45,5 @@ o = DarkCountFockSample(p_dark)
 ev = Event(i,o,interf)
 
 sample!(ev)
-
-# DOESNT WORK but ok if I execute the code of sample copy pasted below
-# n = 3
-# m = 8
-# interf = RandHaar(m)
-# input_state = Input{Bosonic}(first_modes(n,m))
-# o = FockSample()
-#
-# ev = Event(input_state, o, interf)
-#
-# sample!(ev)
-
-#
-# function sample!(ev::Event{TIn, TOut}) where {TIn<:InputType, TOut <: FockSample}
-#
-#     check_probability_empty(ev)
-#
-#     if TIn == Distinguishable
-#         ev.output_measurement.s = ModeOccupation(classical_sampler(ev))
-#     elseif TIn == Bosonic
-#         ev.output_measurement.s = ModeOccupation(cliffords_sampler(ev))
-#     else
-#         error("not implemented")
-#
-#     end
-#
-# end
-#
-# function do_with_probability(p)
-#
-#     """returns true with probability p, false with (1-p)"""
-#
-#     rand() < p ? true : false
-#
-# end
+# output:
+# state = [3, 1, 0, 3, 0, 1, 2, 0, 0, 0]
