771827ba5d0fb4f4ae1e1f2d11545ae04d948189
benoitseron <benoitseron@gmail.com>
2022-09-06 11:33:26 +0200

    some more

--- docs/publication/partitions/efficiency.jl
+++ docs/publication/partitions/efficiency.jl
@@ -59,7 +59,7 @@ end
 n_subsets = 2
 x_array = [0.9,0.95,0.99]
 
-n_max = 20
+n_max = 16
 ########### const density #############
 
 for x in x_array
@@ -105,10 +105,63 @@ for x in x_array
 
     end
 
-    scatter(n_array, tvd_array, yerr = sqrt.(tvd_var_array))
+    scatter(n_array, tvd_array, yerr = sqrt.(tvd_var_array),  yaxis =:log10)
     xlabel!("n")
     ylabel!("tvd B, x = $x")
     title!("n = m^2")
     savefig("tvd_equilibrated_partition_real_average_x(x, m, n_subsets, n) no collision const x = $x.png")
+
+end
+
+########### const density log log #############
+
+for x in x_array
+
+    @show x
+    n_array = collect(5:n_max)
+    m_array = n_array
+    tvd_array = []
+    tvd_var_array = []
+
+    for (n,m) in zip(n_array, m_array)
+
+        t, v = tvd_equilibrated_partition_real_average_x(x, m, n_subsets, n)
+        push!(tvd_array, t)
+        push!(tvd_var_array, v)
+
+    end
+
+    scatter(n_array, tvd_array, yerr = sqrt.(tvd_var_array), yaxis = :log10, xaxis =:log10)
+    xlabel!("n")
+    ylabel!("tvd B, x = $x")
+    title!("n = m")
+    savefig("tvd_equilibrated_partition_real_average_x(x, m, n_subsets, n) const density log log const x = $x.png")
+
+end
+
+
+########### no collision density #############
+
+for x in x_array
+
+    @show x
+    n_array = collect(5:n_max)
+    m_array = n_array .^2
+    tvd_array = []
+    tvd_var_array = []
+
+    for (n,m) in zip(n_array, m_array)
+
+        t, v = tvd_equilibrated_partition_real_average_x(x, m, n_subsets, n)
+        push!(tvd_array, t)
+        push!(tvd_var_array, v)
+
+    end
+
+    scatter(n_array, tvd_array, yerr = sqrt.(tvd_var_array),  xaxis =:log10,  yaxis =:log10)
+    xlabel!("n")
+    ylabel!("tvd B, x = $x")
+    title!("n = m^2")
+    savefig("tvd_equilibrated_partition_real_average_x(x, m, n_subsets, n) no collision log log const x = $x.png")
 
 end
