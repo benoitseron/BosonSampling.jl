cd6564c8702845aab4c20c7faf628fa19ba79a59
Antoine Restivo <antoinerestivo@hotmail.fr>
2022-09-06 13:42:05 +0200

    fixed williamson_decomp and added tools for GBS

--- src/boson_samplers/tools.jl
+++ src/boson_samplers/tools.jl
@@ -1,23 +1,73 @@
+"""
+    duplicate_row_col(A::Array, lengths::Vector{Int})
+
+Returns a matrix where the column and row ``i`` of ``A`` are repeated ``lengths_i`` times.     
+"""
+function duplicate_row_col(A::Array, lengths::Vector{Int})
+
+    x = axes(A,1)
+    @argcheck length(x) == length(lengths)
+
+    res = similar(x, sum(lengths))
+    i = 1
+
+    for idx in 1:length(x)
+        tmp = x[idx]
+        for kdx in 1:lengths[idx]
+            res[i] = tmp
+            i += 1
+        end
+    end
+
+    if length(size(A)) == 2
+        return A[res,res]
+    else
+        return res
+    end
+
+end
+
+"""
+    williamson_decomp(V::AbstractMatrix)
+
+Find the Williamson decomposition of the positive semidefinite matrix ``V``.
+Returns ``D`` and ``S`` such that ``V = S D S^T
+!!! Reference
+    [The Walrus documentation](https://the-walrus.readthedocs.io/en/latest/code/decompositions.html?highlight=williamson#thewalrus.decompositions.williamson)
+"""
 function williamson_decomp(V::AbstractMatrix)
     @argcheck isposdef(V) && size(V)[1] == size(V)[2] && size(V)[1] % 2 == 0
 
-    n = div(size(V)[1], 2)
+    n = div(LinearAlgebra.checksquare(V), 2)
     J = symplectic_mat(n)
-    v = sqrt(inv(V))
-    id = Matrix{Float64}(I, 2n, 2n)
+    v = real.(sqrt(inv(V)))
+    inter = v * J * v
 
-    F = schur(v*J*v)
-    d = F.values
-    select = [(i>0) for i in imag(d)]
-    ordschur!(F, select)
+    F = schur(inter)
+    T = F.T
+    Z = F.Z
 
-    K = F.Z
-    D = diagm(inv.(F.values))
-    S = inv(transpose(v * K * sqrt(D)))
+    perm = vcat([i for i in 1:2:2n-1], [i+1 for i in 1:2:2n-1])
+    x = [0 1; 1 0]
+    id = Matrix{eltype(V)}(I, 2, 2)
+    seq = []
+    for i in 1:2:2n-1
+        T[i,i+1] > 0 ? push!(seq, id) : push!(seq, x)
+    end
 
-    T = real.(1/2 * S * transpose(S))
-    W = real.(S * (D - 1/2 * id) * transpose(S))
-    return T, W
+    p = seq[1]
+    for i in 2:length(seq)
+        p = direct_sum(p, seq[i])
+    end
+
+    Zp = Z * p
+    Zp = Zp[:, perm]
+    Tp = p * T * p
+    d = [1/Tp[i,i+1] for i in 1:2:2n-1]
+    D = diagm(vcat(d,d))
+
+    S = transpose(inv(v * Zp * sqrt(D)))
+    return D, S
 end
 
 function LaplaceExpansion(perm_mat, full_mat)
