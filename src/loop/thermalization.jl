ba73c6ef1dbf8fdea5bf4ef3291a1aeb5ffe10a2
benoitseron <benoitseron@gmail.com>
2022-10-25 15:37:04 +0200

    apparently working thermalization

--- src/loop/thermalization.jl
+++ src/loop/thermalization.jl
@@ -9,7 +9,7 @@ include("packages_loop.jl")
 
 Defines the transmissivities required for the thermalization scheme.
 """
-η_thermalization(n) = [(i-1)/i for i in 2:n]
+η_thermalization(n) = [1-((i-1)/i)^2 for i in 2:n]
 
 """
 partition_thermalization(m)
@@ -34,8 +34,12 @@ d = Uniform(0,2pi)
 
 params = LoopSamplingParameters(n=n, η = η_thermalization(n), η_loss_bs = nothing, η_loss_lines = nothing, ϕ = ϕ)
 
+pretty_table(build_loop(params).U)
+
+pretty_table(fourier_matrix(3))
+
 psp_b = convert(PartitionSamplingParameters, params)
-psp_d = convert(PartitionSamplingParameters, params)
+psp_d = convert(PartitionSamplingParameters, params) # a way to act as copy
 
 psp_b.part = partition_thermalization(m)
 psp_d.part = partition_thermalization(m)
@@ -58,3 +62,12 @@ n_in = [i for i in 0:n]
 plot(n_in, pdf_bos, label = "B", xticks = n_in)
 plot!(n_in, pdf_dist, label = "D")
 ylims!((0,1))
+
+
+###### theory probabilities ######
+
+pdf_dist_theory(k) = binomial(n,k)/n^k * (1-1/m)^(n-k)
+pdf_bos_theory(k) = sum((-1)^(k+a) * binomial(a,k) * binomial(n,a) * factorial(a) / m^(a) for a in k:n)
+
+plot!(n_in, pdf_dist_theory.(n_in), label = "D theory")
+plot!(n_in, pdf_bos_theory.(n_in), label = "B theory")
