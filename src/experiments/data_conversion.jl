3a2f8c312e64525ecfc0ba56ac07e8a643030f85
benoitseron <benoitseron@gmail.com>
2022-11-09 10:42:40 +0100

    upgrade convert_csv_to_samples to a MultipleCounts

--- src/experiments/data_conversion.jl
+++ src/experiments/data_conversion.jl
@@ -1,7 +1,9 @@
 """
-    convert_csv_to_samples(path_to_file::String, m::Int, input_type = ThresholdModeOccupation, input_format = (input_data) -> ModeList(input_data, m))
+    convert_csv_to_samples(path_to_file::String, m::Int, input_type = ThresholdModeOccupation, input_format = (input_data) -> ModeList(input_data, m), samples_type = MultipleCounts)
 
 Reads a CSV file containing experimental data. Converts it into a collection of samples of type `input_type` following the type of detectors available. `input_format` holds how the data is encoded, for instance as a `ModeList` or `ModeOccupation`.
+
+You can choose the encoding of the samples to be either a Vector of observed events (like detector readings) or a `MultipleCounts` if there are many events.
 
 Defaults work for files written as follow: each line is of form
 
@@ -15,18 +17,38 @@ Example usage:
     convert_csv_to_samples("data/loop_examples/test.csv", 10)
 
 """
-function convert_csv_to_samples(path_to_file::String, m::Int, input_type = ThresholdModeOccupation, input_format = (input_data) -> ModeList(input_data, m))
+function convert_csv_to_samples(path_to_file::String, m::Int, input_type = ThresholdModeOccupation, input_format = (input_data) -> ModeList(input_data, m), samples_type = MultipleCounts)
 
     data = readdlm(path_to_file, ',', Int)
 
     samples = Vector{input_type}()
 
-    for (output_number, output_pattern) in enumerate(eachrow(data[:,2:end]))
-        for count in 1:data[output_number,1]
-            push!(samples, input_type(input_format(convert(Array{Int},output_pattern))))
+    if samples_type == Vector
+
+        for (output_number, output_pattern) in enumerate(eachrow(data[:,2:end]))
+            for count in 1:data[output_number,1]
+                push!(samples, input_type(input_format(convert(Array{Int},output_pattern))))
+            end
         end
+
+        return samples
+
+    elseif samples_type == MultipleCounts
+
+        counts = Vector{Real}()
+
+        for (output_number, output_pattern) in enumerate(eachrow(data[:,2:end]))
+
+                push!(samples, input_type(input_format(convert(Array{Int},output_pattern))))
+                push!(counts, data[output_number,1])
+
+        end
+
+        return MultipleCounts(samples, counts ./ sum(counts)) # normalize to make it a probability as is eaten by MultipleCounts
+    else
+        error("not implemented")
     end
 
-    samples
+    return nothing
 
 end
