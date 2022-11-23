3e4a461d2212f2a4ddcf220e7e8a96d7792fb109
benoitseron <benoitseron@gmail.com>
2022-11-23 14:36:56 +0100

    update coincidence code

--- src/experiments/coincidences.jl
+++ src/experiments/coincidences.jl
@@ -8,14 +8,24 @@ Takes in a `MultipleCount` containing event with different number of photons det
 function get_fixed_number_coincidences(summarized_counts::MultipleCounts, number_coincidences::Int)
 
         proba = Vector{Real}()
-        counts = Vector{ModeOccupation}()
+        counts = Vector{eltype(summarized_counts.counts)}()
 
         for i in 1:length(summarized_counts.proba)
 
-                if sum(summarized_counts.counts[i].state) == number_coincidences
+                if typeof(summarized_counts.counts[1]) == ModeOccupation
+                        state = summarized_counts.counts[i].state
+                elseif typeof(summarized_counts.counts[1]) == ThresholdModeOccupation
+                        state = summarized_counts.counts[i].clicks
+                end
+
+                if sum(state) == number_coincidences
                         push!(proba, summarized_counts.proba[i])
                         push!(counts, summarized_counts.counts[i])
                 end
+        end
+
+        if length(proba) == 0
+                @warn "empty counts for number_coincidences = $number_coincidences"
         end
 
         MultipleCounts(counts, proba)
