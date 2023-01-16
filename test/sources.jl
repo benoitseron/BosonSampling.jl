8489eae37c9b1cad8d7e77d98cf414ee73970354
benoitseron <benoitseron@gmail.com>
2023-01-16 13:54:06 +0100

    compute_probability_imperfect_source

--- test/sources.jl
+++ test/sources.jl
@@ -11,4 +11,23 @@
     @test input_probability([1,1,0,1], [1,0,0,1], QuantumDot(efficiency=1)) == 0
     @test input_probability([1,1,0,1], [1,1,0,1], QuantumDot(efficiency=0)) == 0
 
+    # now for imperfect source probabilities
+
+
+    n = 3
+    interf = Fourier(n)
+    o = FockDetection(ModeOccupation([1,1,0]))
+
+    source = QuantumDot(efficiency = 0.4)
+
+    params = SamplingParameters(n = n, interf = interf, o = o)
+
+    set_parameters!(params)
+
+    # test that compute_probability!(params) fails
+
+    @test_throws ErrorException compute_probability!(params)
+    # this doesn't work as there are some lost photons in this lossless interferometer
+
+    compute_probability_imperfect_source(params, source) = 0.09600000000000009
 end
