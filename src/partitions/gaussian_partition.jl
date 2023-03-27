e7f03e11976e0e07d155088ae5d843aa5f22eb09
benoitseron <benoitseron@gmail.com>
2023-03-27 19:40:52 +0200

    fixed marginals

--- src/partitions/gaussian_partition.jl
+++ src/partitions/gaussian_partition.jl
@@ -18,92 +18,8 @@ begin
     using JLD
     using AutoHashEquals
     using LinearRegression
+    
     using DataStructures
-    using Parameters
-    using UnPack
-    using Optim
-
-
-    using Permanents
-    using Plots
-    using Test
-    using Combinatorics
-    using Random
-    using IterTools
-    using Statistics
-    using LinearAlgebra #removed so as to be able to use generic types such as BigFloats, can be put back if needed
-    using PolynomialRoots
-    using StatsBase
-    #using JLD
-    using CSV
-    using DataFrames
-    using Tables
-    using Plots#; plotly() #plotly allows to make "dynamic" plots where you can point the mouse and see the values, they can also be saved like that but note that this makes them heavy (and html instead of png)
-    using PrettyTables #to display large matrices
-    using Roots
-    using BenchmarkTools
-    using Optim
-    using ProgressMeter
-    using ProgressBars
-    using Parameters
-    using ArgCheck
-    using Distributions
-    using Luxor
-    using AutoHashEquals
-    using LinearRegression
-    using HypothesisTests
-    using SimpleTraits
-    using Parameters
-    using UnPack
-    using Dates
-    using JLD
-    using DelimitedFiles
-    using BosonSampling
-    using Plots
-    using ProgressMeter
-    using Distributions
-    using Random
-    using Test
-    using ArgCheck
-    using StatsBase
-    using ColorSchemes
-    using Interpolations
-    using Dierckx
-    using LinearAlgebra
-    using PrettyTables
-    using LaTeXStrings
-    using JLD
-    using AutoHashEquals
-    using LinearRegression
-    using DataStructures
-    using Parameters
-    using UnPack
-    using Combinatorics
-    using BosonSampling
-    using Plots
-    using ProgressMeter
-    using Distributions
-    using Random
-    using Test
-    using ArgCheck
-    using ColorSchemes
-    using Interpolations
-    using Dierckx
-    using LinearAlgebra
-    using PrettyTables
-    using LaTeXStrings
-    using JLD
-    using AutoHashEquals
-    using LinearRegression
-    using DataStructures
-    using Parameters
-    using UnPack
-    using Optim
-    using DelimitedFiles
-    using Dates
-    using DelimitedFiles    
-
-    using ITensors
 end
 
 
@@ -152,14 +68,14 @@ C = diagm(C_array)
 
 part = equilibrated_partition(m, 1)
 
-# part = BosonSampling.Partition(Subset([1]))
-# part.subsets[1].subset
+part = Partition(Subset(first_modes(1, m)))
+part.subsets[1].subset
 
 
 
 ### cutoff ###
 
-n_max = 10
+n_max = 40
 @warn "arbitrary cutoff"
 
 
