6f64390bde7c50c3efd9b7ab127c318377da56bb
benoitseron <benoitseron@gmail.com>
2023-06-20 15:14:21 +0200

    starting

---
+++ src/partitions/gabrielle_trials.jl
@@ -0,0 +1,23 @@
+include("gaussian_partition.jl")
+
+
+##### conventions #####
+
+# r - squeezing
+# x - generatinc funtion
+# η - vector of fourier coefficients
+# m - modes
+# Q - 
+# Λ -
+# δ -
+# λ - thermal parameters
+# V = U_round
+
+# the lambdas are created page 24
+# Q page 23
+# C just above
+
+
+##### your previous formalism ######
+
+
