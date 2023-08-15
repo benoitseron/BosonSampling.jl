feaa236a8ddc03da5b258e5c023969ef1cf4dd17
benoitseron <benoitseron@gmail.com>
2023-08-15 11:49:40 +0200

    with error bars

--- src/partitions/asymptotics.jl
+++ src/partitions/asymptotics.jl
@@ -1,83 +1,90 @@
 include("gaussian_partition.jl")
 
-### binning demo ###
+### Haar averaged binning demo ###
 
-# overall bins:
+n_iter = 100
+
+m = 20
+input_state = GeneralGaussian(m = m, r = 0.4 * ones(m))
+
+part = equilibrated_partition(m, 2)
+
+n_max = 10
 
 begin
+    
+    mc_array = []
 
-    m = 20
-    input_state = GeneralGaussian(m = m, r = 0.4 * ones(m))
-    interferometer = RandHaar(m)
-    part = equilibrated_partition(m, 2)
+    @showprogress for i in 1:n_iter
 
-    # part = Partition(Subset(first_modes(1, m)))
-    # part.subsets[1].subset
+        interferometer = RandHaar(m)
+        mc_iter = compute_probabilities_partition_gaussian(interferometer, part, input_state, n_max)
 
-    n_max = 30
-    mc = compute_probabilities_partition_gaussian(interferometer, part, input_state, n_max)
+        # bar(real(pdf))
 
-    # bar(real(pdf))
+        sort_samples_total_photon_number_in_partition!(mc_iter)
+        push!(mc_array, mc_iter)
 
-    sort_samples_total_photon_number_in_partition!(mc)
-
-    @show mc
-
-    bar(mc.proba)
+    end
 
 end
 
+averaged_proba = vec(mean(hcat([mc_array[i].proba for i in 1:n_iter]), dims = 1))[1]
+deviation = vec(sqrt.(var(hcat([mc_array[i].proba for i in 1:n_iter]...), dims = 2)))
+
+mc = MultipleCounts(mc_array[1].counts, averaged_proba)
+mc_deviation = MultipleCounts(mc_array[1].counts, deviation)
 
 mc_partition_expectation_values_array_bosonic, mc_partition_expectation_values_array_dist = partition_expectation_values_gaussian(input_state, n_max, part)
 
 bar(mc_partition_expectation_values_array_bosonic.proba, alpha = 0.5, label = L"Indistinguishable")
 bar!(mc_partition_expectation_values_array_dist.proba, alpha = 0.5, label = L"Distinguishable")
 
-### comparison with numerics ###
+# ### comparison with numerics ###
 
-mc, probas_fourier = compute_probabilities_partition_gaussian_chicago(interferometer, part, input_state, n_max, give_debug_info = true)
+# mc, probas_fourier = compute_probabilities_partition_gaussian_chicago(interferometer, part, input_state, n_max, give_debug_info = true)
 
-sort_samples_total_photon_number_in_partition!(mc)
+# sort_samples_total_photon_number_in_partition!(mc)
 
-range = 1:50
+# range = 1:50
 
-bar(real.(mc.proba[range]), alpha = 0.5, label = L"Numerics")
-bar!(mc_partition_expectation_values_array_bosonic.proba[range], alpha = 0.5, label = L"Indistinguishable")
-bar!(mc_partition_expectation_values_array_dist.proba[range], alpha = 0.5, label = L"Distinguishable")
+# bar(real.(mc.proba[range]), alpha = 0.5, label = L"Numerics")
+# bar!(mc_partition_expectation_values_array_bosonic.proba[range], alpha = 0.5, label = L"Indistinguishable")
+# bar!(mc_partition_expectation_values_array_dist.proba[range], alpha = 0.5, label = L"Distinguishable")
 
-### separating events by photon number ###
+# ### separating events by photon number ###
 
-sorted_counts_numerics = sort_by_detected_photons(mc)
-sorted_counts_bosonic = sort_by_detected_photons(mc_partition_expectation_values_array_bosonic)
-sorted_counts_dist = sort_by_detected_photons(mc_partition_expectation_values_array_dist)
+# sorted_counts_numerics = sort_by_detected_photons(mc)
+# sorted_counts_bosonic = sort_by_detected_photons(mc_partition_expectation_values_array_bosonic)
+# sorted_counts_dist = sort_by_detected_photons(mc_partition_expectation_values_array_dist)
 
-n_detected = 10
+# n_detected = 10
 
-scatter(sorted_counts_numerics[n_detected].proba, alpha = 0.5, label = L"Numerics")
-scatter!(sorted_counts_bosonic[n_detected].proba, alpha = 0.5, label = L"Indistinguishable")
-scatter!(sorted_counts_dist[n_detected].proba, alpha = 0.5, label = L"Distinguishable")
+# scatter(sorted_counts_numerics[n_detected].proba, alpha = 0.5, label = L"Numerics")
+# scatter!(sorted_counts_bosonic[n_detected].proba, alpha = 0.5, label = L"Indistinguishable")
+# scatter!(sorted_counts_dist[n_detected].proba, alpha = 0.5, label = L"Distinguishable")
 
-###### to plot a mc ######
-
-
-dpi_value = 600
-fig_width = 1200  # in pixels
-fig_height = 500  # in pixels
+# ###### to plot a mc ######
 
 
-range = 1:80
-mc_reduced = MultipleCounts(mc.counts[range], mc.proba[range])
+# dpi_value = 600
+# fig_width = 1200  # in pixels
+# fig_height = 500  # in pixels
 
-x_data = mc_reduced.counts
-y_data = mc_reduced.proba
 
-function plot_string_repr(i::ModeOccupation)
-    return string(i.state)
-end
+# range = 1:80
+# mc_reduced = MultipleCounts(mc.counts[range], mc.proba[range])
 
-x_labels = map(plot_string_repr, x_data)
+# x_data = mc_reduced.counts
+# y_data = mc_reduced.proba
 
-plot(y_data, xticks=(1:length(x_labels), x_labels), xrotation=60, legend=false, xtickfontsize=8, dpi=dpi_value, size=(fig_width, fig_height))
+# function plot_string_repr(i::ModeOccupation)
+#     return string(i.state)
+# end
+
+# x_labels = map(plot_string_repr, x_data)
+
+# plot(y_data, xticks=(1:length(x_labels), x_labels), xrotation=60, legend=false, xtickfontsize=8, dpi=dpi_value, size=(fig_width, fig_height))
 
 ### large plot ###
 
@@ -91,6 +98,7 @@ mc_reduced = MultipleCounts(mc.counts[range], mc.proba[range])
 
 x_data = mc_reduced.counts
 y_data = mc_reduced.proba
+y_err_data = mc_deviation.proba[range]
 
 function plot_string_repr(i::ModeOccupation)
     return string(i.state)
@@ -100,7 +108,7 @@ x_labels = map(plot_string_repr, x_data)
 
 # Define margins: [left, bottom, right, top]
 
-scatter(y_data, xticks=(1:length(x_labels), x_labels), xrotation=60, legend=false, xtickfontsize=8, ytickfontsize=12, dpi=dpi_value, size=(fig_width, fig_height), alpha = 0.5, label = L"Numerics")
+scatter(y_data, yerr = y_err_data, xticks=(1:length(x_labels), x_labels), xrotation=60, xtickfontsize=8, ytickfontsize=12, dpi=dpi_value, size=(fig_width, fig_height), alpha = 0.5, label = L"Numerics")
 bar!(mc_partition_expectation_values_array_bosonic.proba[range], alpha = 0.5, label = L"Indistinguishable")
 bar!(mc_partition_expectation_values_array_dist.proba[range], alpha = 0.5, label = L"Distinguishable")
 
--- src/partitions/gaussian_partition.jl
+++ src/partitions/gaussian_partition.jl
@@ -277,3 +277,143 @@ function partition_expectation_values_gaussian(input_state::GeneralGaussian, n_m
 
 end
 
+
+
+function compute_probabilities_partition_gaussian_chicago(physical_interferometer::Interferometer, part::Partition, input_state::GeneralGaussian, n_max = 11; give_debug_info = false)
+
+
+        if !all(input_state.displacement .== 0.0)
+            error("displacement not implemented")
+        end
+    
+    
+        if n_max % 2 == 0
+            @warn "n_max must be odd for FFT purposes, converting"
+            n_max = n_max +1
+        end
+    
+        physical_interferometer = interferometer
+        ### function ###
+    
+        @warn "arbitrary cutoff"
+    
+        # unpack the parameters of the input state
+    
+        @unpack m, r, λ, delta_x, delta_y = input_state
+    
+    
+        renormalization_factor = 4^m 
+        @warn "normalization factor should be one - to debug!"
+    
+    
+    
+        γ = @. [-1 + exp(2*r[j]) for j in 1:m]
+    
+        U = physical_interferometer.U
+    
+        ### useful matrices ###
+    
+        Γ = diagm(γ)
+        N = prod(@. sqrt(1+ γ) / (π * γ))
+    
+        ### virtual interferometer matrix ###
+    
+        fourier_indexes = all_mode_configurations(n_max,part.n_subset, only_photon_number_conserving = false)
+        probas_fourier = Array{ComplexF64}(undef, length(fourier_indexes))
+        top_right_matrix = similar(U)
+        bottom_left_matrix = similar(U)
+    
+        Q_non_psd = []
+    
+        for (index_fourier_array, fourier_index) in enumerate(fourier_indexes)
+    
+            # for each fourier index, we recompute the virtual interferometer
+            top_right_matrix  = conj(U)
+            bottom_left_matrix = U
+    
+            diag = [0.0 + 0im for i in 1:m] # previously 1.0 + 0im
+            
+            for (i,fourier_element) in enumerate(fourier_index)
+    
+                    # @show fourier_element
+    
+                    this_phase = exp(2*pi*1im/(n_max+1) * fourier_element)
+    
+                    # @show this_phase
+    
+                    for j in 1:length(diag)
+                            # @show i,j
+                            if part.subsets[i].subset[j] == 1
+    
+                                    diag[j] = this_phase 
+    
+                            end
+    
+                    end
+    
+            end
+    
+            # @show diag 
+    
+            top_right_matrix *= Diagonal(diag)
+            bottom_left_matrix *= Diagonal(diag)
+    
+            top_right_matrix *= - conj(U') # more practical than the transpose function 
+            bottom_left_matrix *= - U' ################ check minus sign
+    
+            ### matrix Q ###
+    
+            Q = zeros(ComplexF64, 2m, 2m)
+    
+            Q[1:m, 1:m] = 2 * Γ^(-1) + diagm([1.0 + 0im for i in 1:m])
+            Q[1:m, m+1:2m] = top_right_matrix
+            Q[m+1:2m, 1:m] = bottom_left_matrix
+            Q[m+1:2m, m+1:2m] = 2 * Γ^(-1) + diagm([1.0 + 0im for i in 1:m])
+    
+            if !is_positive_semidefinite(real.(Q))
+                @warn "real part of Q is not positive semidefinite for fourier index $fourier_index"
+                push!(Q_non_psd, Q)
+            end
+    
+            probas_fourier[index_fourier_array] = N * (2\pi)^(m) * det(Q)^(-0.5)
+            
+        end
+    
+        if Q_non_psd != []
+            @warn "the real part of Q is not positive semidefinite for some fourier indexes"
+    
+            for Q in Q_non_psd
+                @show Q
+                @show eigvals(Q)
+            end
+    
+            return nothing
+        end
+    
+        physical_indexes = copy(fourier_indexes)
+    
+        probas_fourier_matrix = reshape(probas_fourier, ((n_max + 1), div(length(probas_fourier), (n_max + 1))))
+    
+        shifted_probas_fourier_matrix = fftshift(probas_fourier_matrix)
+        pdf_matrix = fft(shifted_probas_fourier_matrix) / (n_max + 1)^part.n_subset
+    
+        pdf = renormalization_factor * reshape(pdf_matrix, (length(probas_fourier),))
+    
+        try
+            pdf = clean_pdf(pdf)
+        catch
+            @warn "invalid pdf, skipping cleaning"
+            # println("press any key to continue")
+            # readline()
+            pdf = real.(pdf)
+        end
+    
+        mc = MultipleCounts(ModeOccupation.(physical_indexes), pdf)
+    
+        if give_debug_info
+            return mc, probas_fourier, pdf_matrix, shifted_probas_fourier_matrix, pdf
+        else
+            return mc
+        end
+    
+    end
