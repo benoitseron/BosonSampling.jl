2f34685f2157639a84c3acbad0b8eab886276745
Benoit Seron <benoitseron@gmail.com>
2024-06-18 14:17:22 +0200

    better spoofing images

---
+++ docs/publication/partitions/spoofing_trials.jl
@@ -0,0 +1,175 @@
+using Revise
+
+using BosonSampling
+using Plots
+using ProgressMeter
+using Distributions
+using Random
+using Test
+using ArgCheck
+using StatsBase
+using ColorSchemes
+using Interpolations
+using Dierckx
+using LinearAlgebra
+using PrettyTables
+using LaTeXStrings
+using JLD
+using AutoHashEquals
+using LinearRegression
+
+using DataStructures 
+
+cd("docs/publication/partitions/")
+
+n_iter_each_unitary = 100
+
+
+function tvd_two_partitions(n,m,n_subsets, n_iter_each_unitary = n_iter_each_unitary)
+    ib = Input{Bosonic}(first_modes(n,m))
+    interf = RandHaar(m)
+
+    tvd_array_this_unitary = zeros(n_iter_each_unitary)
+
+    for i in 1:n_iter_each_unitary
+        part_a = random_partition(m,n_subsets)
+        part_b = part_a
+        while part_a == part_b
+            part_b = random_partition(m,n_subsets)
+        end
+
+        o_a = PartitionCountsAll(part_a)
+        o_b = PartitionCountsAll(part_b)
+
+        ev_a = Event(ib,o_a,interf)
+        ev_b = Event(ib,o_b,interf)
+
+        compute_probability!(ev_a)
+        compute_probability!(ev_b)
+
+        tvd_array_this_unitary[i] = tvd(ev_a.proba_params.probability.proba, ev_b.proba_params.probability.proba)
+
+    end
+
+    return mean(tvd_array_this_unitary)
+
+end
+
+function variation_two_partitions(n,m, n_subsets, n_iter)
+    tvd_array = zeros(n_iter)
+    for i in 1:n_iter
+        tvd_array[i] = tvd_two_partitions(n,m,n_subsets)
+    end
+
+    mean_tvd = mean(tvd_array)
+    std_tvd = std(tvd_array)
+    return mean_tvd, std_tvd
+end
+
+n_array = 2:1:12
+m_array = n_array
+n_subsets_array = 2:3
+n_iter = 100
+
+plt = plot(dpi = 600)
+
+tvd_arrays_high_density = []
+std_tvd_arrays_high_density = []
+
+for n_subset in n_subsets_array
+    mean_tvd_array = []
+    std_tvd_array = []
+
+    
+    for (i,n) in enumerate(n_array)
+        if n_subset <= n
+
+            @show (n,m_array[i], n_subset, n_iter)
+            mean_tvd, std_tvd = variation_two_partitions(n,m_array[i], n_subset, n_iter)
+            push!(mean_tvd_array, mean_tvd)
+            push!(std_tvd_array, std_tvd)
+
+        else
+
+            push!(mean_tvd_array, NaN)
+            push!(std_tvd_array, NaN)
+        end
+    end
+
+    push!(tvd_arrays_high_density, mean_tvd_array)
+    push!(std_tvd_arrays_high_density, std_tvd_array)
+
+    plot!(plt, n_array, mean_tvd_array, ribbon = std_tvd_array, label = "n_subsets = $n_subset")
+end
+
+
+xlabel!(plt, L"n")
+ylabel!(plt, L"tvd")
+title!(plt, "High density regime " * L"(m = n)")
+ylims!(plt, (0,2))
+xticks!(n_array)
+plot!(plt, legend = false)
+# title!("spoofabilitiy - no collision regime")
+plt
+
+# savefig(plt, "spoofing_no_collision.png")
+savefig(plt, "./images/publication/spoofing_high_density.png")
+
+
+n_array = 2:1:12
+m_array = n_array .^2
+n_subsets_array = 2:3
+n_iter = 100
+
+plt_2 = plot(dpi = 600)
+
+tvd_arrays_no_collision = []
+std_tvd_arrays_no_collision = []
+
+
+
+for n_subset in n_subsets_array
+    mean_tvd_array = []
+    std_tvd_array = []
+
+    
+    for (i,n) in enumerate(n_array)
+        if n_subset <= n
+
+            @show (n,m_array[i], n_subset, n_iter)
+            mean_tvd, std_tvd = variation_two_partitions(n,m_array[i], n_subset, n_iter)
+            push!(mean_tvd_array, mean_tvd)
+            push!(std_tvd_array, std_tvd)
+
+        else
+
+            push!(mean_tvd_array, NaN)
+            push!(std_tvd_array, NaN)
+        end
+    end
+
+    push!(tvd_arrays_no_collision, mean_tvd_array)
+    push!(std_tvd_arrays_no_collision, std_tvd_array)
+
+    plot!(plt_2, n_array, mean_tvd_array, ribbon = std_tvd_array, label = "n_subsets = $n_subset")
+end
+
+xlabel!(L"n")
+ylabel!(L"tvd")
+# title!("spoofabilitiy - high density regime")
+title!("No collision regime " * L"(m = n^2)")
+ylims!(0,2)
+xticks!(n_array)
+plt_2
+
+savefig(plt_2, "./images/publication/spoofing_no_collision.png")
+# savefig(plt, "./images/publication/spoofing_high_density.png")
+
+full_plot = plot(plt, plt_2, layout = (2, 1), dpi = 600)
+
+savefig(full_plot, "./images/publication/spoofing.png")
+
+save("spoofing_tvd_arrays_high_density.jld", "tvd_arrays_high_density", tvd_arrays_high_density)
+save("spoofing_std_tvd_arrays_high_density.jld", "std_tvd_arrays_high_density", std_tvd_arrays_high_density)
+save("spoofing_tvd_arrays_no_collision.jld", "tvd_arrays_no_collision", tvd_arrays_no_collision)
+save("spoofing_std_tvd_arrays_no_collision.jld", "std_tvd_arrays_no_collision", std_tvd_arrays_no_collision)
